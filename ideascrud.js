f2bb06b3897995c15f93b5aabfff824c4129fab2
JoelR111 <joel.roberts2006@gmail.com>
2025-06-02 22:00:46 +0100

    split up each item into title,desc and status - added multiple statuses + styling

--- ideascrud.js
+++ ideascrud.js
@@ -42,18 +42,83 @@ function Idea(idea, desc){
 
 function displayIdeas(currentItem){
     const li = document.createElement("li");
-    li.textContent = currentItem.title + " - " + currentItem.detail + " - " + currentItem.state;
-
     li.className = "idea-item";
+    
+
+    //Split elements seperate
+
+    //Idea Name
+    const ideanameP = document.createElement("p");
+    ideanameP.textContent = currentItem.title;
+
+    //Idea Desc
+    const ideadescP = document.createElement("p");
+    ideadescP.textContent = currentItem.detail;
+
+    //Idea Status
+    //Create element (dropdown)
+    const ideastatus = document.createElement("select");
+    const statusOptions = ["Not Started", "Interested", "Developing", "Completed"];
+
+    //Loop through each of the options (adding them to the dropdown options).
+    statusOptions.forEach((optionText) =>{
+        const option = document.createElement("option");
+        option.value = optionText;
+        option.textContent = optionText;
+
+        //Check if currentItem (title, detail and ->(state)) matches the one of the state options in the select.
+        if(optionText === currentItem.state){
+            //set the selected option to true
+            option.selected = true;
+        }
+        ideastatus.appendChild(option);
+        
+
+    });
+
+    statusColor(ideastatus.value);
+
+    //Update items status in array on change
+    ideastatus.addEventListener("change", function(){
+        currentItem.state = ideastatus.value;
+        statusColor(ideastatus.value);
+    });
+
+
+    //add to li
+    li.appendChild(ideanameP);
+    li.appendChild(ideadescP);
+    li.appendChild(ideastatus);
+
+    //add li to idea subcontainer
     const ideasListSub = document.getElementById("idea-subcontainer");
     ideasListSub.appendChild(li)
 
+
+    function statusColor(currentStatus){
+        
+        ideastatus.className = "";
+
+        if(currentStatus === "Not Started"){
+            ideastatus.classList.add("status-select-notstarted");  
+        }
+        else if(currentStatus === "Interested"){
+            ideastatus.classList.add("status-select-interested");
+        }
+        else if(currentStatus === "Developing"){
+            ideastatus.classList.add("status-select-developing");
+        }
+        else if(currentStatus === "Completed"){
+            ideastatus.classList.add("status-select-completed");
+        };
+    };
 };
 
 function clearIdeaEntry(){
     document.getElementById("ideaname-entry").value = "";
     document.getElementById("ideadesc-entry").value="";
 };
+
 
 
 
